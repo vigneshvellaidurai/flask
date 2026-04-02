3e700021159425677a500c2e2284114c09cd66c3
vigneshvellaidurai <vignesh.vellaidurai@gmail.com>
2026-04-02 10:46:37 +0530

    web-page-add1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "from vicky"
+    return render_template("index.html")    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
